2905577fa82b63f4d05062786db642e3b6eaec16
memphison <jonathandunavant@gmail.com>
2026-01-09 17:14:45 -0500

    m

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -541,14 +541,15 @@ setAisVessels(filtered);
       })
     : events;
 
-return [...aisUnderwayEvents, ...filteredEvents];
+return filteredEvents;
+
 
 }, [events, aisVessels]);
 
 
   return (
     <main style={{ padding: 24, fontFamily: "system-ui", maxWidth: 880 }}>
-      <h1 style={{ margin: 0, color: theme.pageText }}>The Waving Girl-test</h1>
+      <h1 style={{ margin: 0, color: theme.pageText }}>The Waving Girl</h1>
 
       <p style={{ marginTop: 8, color: theme.subText }}>
         Big ships moving on the Savannah River in the {windowLabel}.
